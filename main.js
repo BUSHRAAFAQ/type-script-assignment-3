4e0fb19b2ce87bb18373000f1b90a02428da5051
Bushra Afaq <afaqbushra38@gmail.com>
2024-10-02 23:59:38 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function describe_city(city, country = "Pakistan") {
+    console.log(`${city} is in the ${country}`);
+}
+describe_city("Lahore");
+describe_city("Karachi");
+describe_city("Dobia", "middle East");
---
+++ main.ts
@@ -0,0 +1,6 @@
+function describe_city(city:string,country:string = "Pakistan"){
+console.log(`${city} is in the ${country}`);
+}
+describe_city("Lahore");
+describe_city("Karachi");
+describe_city("Dobia","middle East");
